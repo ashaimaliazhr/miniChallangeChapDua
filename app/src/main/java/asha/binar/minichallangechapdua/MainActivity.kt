060b9d648ab12d203b6b7597e3bbde87cc7bc9f9
ashaimaliazhr <asha.imaliazahra2006@students.unila.ac.id>
2022-08-20 21:12:57 +0700

    Initial commit

---
+++ app/src/main/java/asha/binar/minichallangechapdua/MainActivity.kt
@@ -0,0 +1,11 @@
+package asha.binar.minichallangechapdua
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
